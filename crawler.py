3288f60c6c755c90d3b7ee4719af2342ba56b407
Loc Ngo <xuanloc0511@gmail.com>
2025-02-05 17:34:23 -0500

    feat(selenium) switch timeout to 30s
    Signed-off-by: Loc Ngo <xuanloc0511@gmail.com>

--- crawler.py
+++ crawler.py
@@ -40,11 +40,11 @@ def bypass_captcha(url) -> str:
             has_results = True
         except NoSuchElementException as e:
             logger.warning(f"not found search-results, looks like it is blocked by captcha")
-            WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
+            WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it(
                 (By.CSS_SELECTOR, "iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 30).until(
                 EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located(
                 (By.XPATH, "//div[@class='search-results']")
             ))
         return driver.page_source
